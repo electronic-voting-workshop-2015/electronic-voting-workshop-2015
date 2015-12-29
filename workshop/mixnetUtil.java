832ff030f9bab8158989656d10c11b3a4db64c46
arbeladmoni <arbel.admoni@gmail.com>
2015-12-29 23:41:36 +0200

    added documention

--- workshop/mixnetUtil.java
+++ workshop/mixnetUtil.java
@@ -1,6 +1,8 @@
 package workshop;
 
-  \*
-  *
-  *\
+  /*
+  * takes data from source (its a URL path for the BB).
+  * upload the prof and new votes to the dest path.
+  * sign with the privateSig Key
+  */
   public void Mixnet(String source, String dest, int privateSig);
