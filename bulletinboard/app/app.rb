67d0462f4356ff1941e1a55a297908726a41aece
Moran <moran@moran.moran>
2016-02-12 16:21:14 +0200

    bulletin board web-app entrypoints with place-holder values

--- bulletinboard/app/app.rb
+++ bulletinboard/app/app.rb
@@ -1,14 +1,24 @@
 require 'sinatra'
+require 'sinatra/json'
 require 'rack'
 require 'rack/contrib'
 require 'pp'
 
 use Rack::PostBodyContentTypeParser
 
-get '/' do
-    $lastVote
+get '/getVotingDomainParameters' do
+   json( { group_type: "group-type-here", parameters: { param1: 1, param2: 2 } } )
+end
+
+get '/getVotingPublicKey' do
+    json :voting_public_key => "voting_public_key here"
+end
+
+get '/getSignaturePublicKey/:kalpi_id' do
+    kalpi_id = params[ "kalpi_id" ]
+    json :kalpi_id => kalpi_id, public_key: "PUBLIC KEY FOR #{kalpi_id}"
 end
 
 post '/sendVote' do
-    $lastVote = "received vote,  parameters were: id: #{params["id"]} data: #{params["data"]}\n"
+    json :parameters_were => params
 end
