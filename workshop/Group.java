421032f01bc26e881404b074e796e431d7876519
NoaFeldman <feld.noa@gmail.com>
2016-02-17 13:51:37 +0200

    Update Group.java
    Remove public generator. elementSize and order, and leave this fields with getters and setters.

--- workshop/Group.java
+++ workshop/Group.java
@@ -3,12 +3,6 @@ package workshop;
  * We only use the group interface, the cryptographic infrastructure should recode it to their dedicated use
  */
 public abstract class Group{
-	//  groupOrder is the order of the chosen group	
-	public byte[] groupOrder;
-	//  generator is the pre-chosen generator
-	public byte[] generator;
-	//   elementBitSize is the size in bits of each element in the group
-	public int elementBytesSize;
 	/**
 	 * Returns the generator exponated by 'exponent'
 	 * @param exponent
