da980126dd94f7827b78d1f4f98ebf857f947644
Moran <moran@moran.moran>
2016-02-21 22:49:55 +0200

    removed unneeded things

--- bulletinboard/app/app.rb
+++ bulletinboard/app/app.rb
@@ -8,19 +8,6 @@ require 'models/proof'
 
 use Rack::PostBodyContentTypeParser
 
-get '/getVotingDomainParameters' do
-   json( { group_type: "group-type-here", parameters: { param1: 1, param2: 2 } } )
-end
-
-get '/getVotingPublicKey' do
-    json :voting_public_key => "voting_public_key here"
-end
-
-get '/getSignaturePublicKey/:kalpi_id' do
-    kalpi_id = params[ "kalpi_id" ]
-    json :kalpi_id => kalpi_id, public_key: "PUBLIC KEY FOR #{kalpi_id}"
-end
-
 post '/sendVote' do
     json :parameters_were => params
 end
