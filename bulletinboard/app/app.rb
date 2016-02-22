44773206c2ddcc55395f15701559bd63c77c634e
jschejter <jschejter@gmail.com>
2016-02-22 17:52:40 +0200

    started publishZKP

--- bulletinboard/app/app.rb
+++ bulletinboard/app/app.rb
@@ -17,6 +17,18 @@ post '/publish' do
     json id: publication.id
 end
 
+post '/publishZKP' do
+  data = JSON.parse request.body.read
+  
+  #todo: verify signature
+  
+  voteZkp = ZKP.find(data[:vote_id])
+    update = ZKP.update(data[:vote_id], :"party_#{data[:party_id]}" => data[:zkp])
+  
+rescue ActiveRecord::RecordNotFound
+  create = ZKP.create(vote_id: data[:vote_id], "party_#{data[:party_id]}": data[:zkp])
+end
+
 get '/retrieve' do
     begin
         id = params[ "id" ].to_i
