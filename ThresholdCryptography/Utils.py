b5e0977ffeb028528dc22cb525019dcb74841e91
ilayluz <ilayluz@gmail.com>
2016-02-22 19:06:25 +0200

    Update Utils.py

--- ThresholdCryptography/Utils.py
+++ ThresholdCryptography/Utils.py
@@ -1,10 +1,9 @@
-import math
-import random
-from urllib.request import *
 import json
 import base64
-from itertools import islice
+import urllib.request
 
+
+BB_API_ERROR = "error"  # TODO: define error message from BB
 
 def bits(n):
     """Generates binary digits of n, starting from least significant bit.
@@ -16,7 +15,7 @@ def bits(n):
 
 
 def split_every(n, iterable):
-    """from http://stackoverflow.com/a/1915307"""
+    """from http://stackoverflow.com/a/312464"""
     for i in range(0, len(iterable), n):
         yield iterable[i:i+n]
 
@@ -82,72 +81,12 @@ def base64_to_list(b, member_length=0, curve=None):
     return bytes_to_list(base64_to_bytes(b), member_length, curve)
 
 
-def publish_list(l, int_length, url):
+def publish_list(l, int_length, sender_id, signature, table_id, recipient_id, url):
     """publishes list l to url using POST request"""
     base64_data = list_to_base64(l, int_length)
-
-
-def rabinMiller(n):
-    """https://langui.sh/2009/03/07/generating-very-large-primes/#fn:1"""
-    s = n - 1
-    t = 0
-    while s & 1 == 0:
-        s = s // 2
-        t += 1
-    k = 0
-    while k < 128:
-        a = random.randrange(2, n - 1)
-        # a^s is computationally infeasible.  we need a more intelligent approach
-        # v = (a**s)%n
-        # python's core math module can do modular exponentiation
-        v = pow(a, s, n)  # where values are (num,exp,mod)
-        if v != 1:
-            i = 0
-            while v != (n - 1):
-                if i == t - 1:
-                    return False
-                else:
-                    i = i + 1
-                    v = (v ** 2) % n
-        k += 2
-    return True
-
-
-def isPrime(n):
-    # lowPrimes is all primes (sans 2, which is covered by the bitwise and operator)
-    # under 1000. taking n modulo each lowPrime allows us to remove a huge chunk
-    # of composite numbers from our potential pool without resorting to Rabin-Miller
-    lowPrimes = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
-        , 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179
-        , 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269
-        , 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367
-        , 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461
-        , 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571
-        , 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661
-        , 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773
-        , 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883
-        , 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
-    if (n >= 3):
-        if (n & 1 != 0):
-            for p in lowPrimes:
-                if (n == p):
-                    return True
-                if (n % p == 0):
-                    return False
-            return rabinMiller(n)
-    return False
-
-
-def generateLargePrime(k):
-    # k is the desired bit length
-    r = 100 * (math.log(k, 2) + 1)  # number of attempts max
-    r_ = r
-    while r > 0:
-        # randrange is mersenne twister and is completely deterministic
-        # unusable for serious crypto purposes
-        n = random.randrange(2 ** (k - 1), 2 ** (k))
-        r -= 1
-        if isPrime(n) == True:
-            return n
-    return "Failure after " + repr(r_) + " tries."
-
+    json_data = json.dumps([sender_id, base64_data, signature, table_id, recipient_id])
+    req = urllib.request.Request(url, json_data, {'Content-Type': 'application/json'})
+    with urllib.request.urlopen(req) as response:
+        read_value = response.read()  # read_value is a bytes object
+    if read_value.decode('utf-8') == BB_API_ERROR:
+        return None
