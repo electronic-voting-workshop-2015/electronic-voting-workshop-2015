cfe4d5b5cc4e7e53edb0b1da1b93a626fd8e351b
jschejter <jschejter@gmail.com>
2016-02-22 15:58:18 +0200

    change file name and create global threshold variables

--- bulletinboard/db/migrate/20160221214313_create_tables.rb
+++ bulletinboard/db/migrate/20160221214313_create_tables.rb
@@ -1,10 +1,5 @@
-class CreatePublications < ActiveRecord::Migration
-  def change
-      create_table :publications do |t|
-          t.binary :content
-      end
-  end
-end
+$N = 7
+$T = 5
 
 class CreateTables < ActiveRecord::Migration
   def up
