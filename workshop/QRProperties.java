eac8daa86a4fce6f892014ccb6d926fc346ea970
sofiarov <srsoniarov@gmail.com>
2016-02-24 20:19:48 +0200

    Update QRProperties.java

--- workshop/QRProperties.java
+++ workshop/QRProperties.java
@@ -1,35 +1,53 @@
 package workshop;
 
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
 public class QRProperties {
-		private int level;
-		private ECCLevel ecc;
-		private int maxNumOfBits;
-		public int getLevel() {
-			return level;
-		}
-		public void setLevel(int level) {
-			this.level = level;
-		}
-		public ECCLevel getEcc() {
-			return ecc;
-		}
-		public void setEcc(ECCLevel ecc) {
-			this.ecc = ecc;
-		}
-		public int getMaxNumOfBits() {
-			return maxNumOfBits;
-		}
-		public void setMaxNumOfBits(int maxNumOfBits) {
-			this.maxNumOfBits = maxNumOfBits;
-		}
-		public QRProperties(int level, ECCLevel ecc, int maxNumOfBits){
-			this.level=level;
-			this.ecc=ecc;
-			this.maxNumOfBits=maxNumOfBits;
-		}
-		public QRProperties(){
-			this.level=0;
-			this.ecc=null;
-			this.maxNumOfBits=0;
-		}
+
+	private int level;
+	private ErrorCorrectionLevel ecc;
+	private int maxNumOfBits;
+	private int numberOfPixels;
+
+	public int getNumberOfPixels() {
+		return this.numberOfPixels;
+		//TODO: calculate number of pixels according to QR level.
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public ErrorCorrectionLevel getEcc() {
+		return ecc;
+	}
+
+	public void setEcc(ErrorCorrectionLevel ecc) {
+		this.ecc = ecc;
+	}
+
+	public int getMaxNumOfBits() {
+		return maxNumOfBits;
+	}
+
+	public void setMaxNumOfBits(int maxNumOfBits) {
+		this.maxNumOfBits = maxNumOfBits;
+	}
+
+	public QRProperties(int level, ErrorCorrectionLevel ecc, int maxNumOfBits) {
+		this.level = level;
+		this.ecc = ecc;
+		this.maxNumOfBits = maxNumOfBits;
+	}
+
+	public QRProperties() {
+		this.level = 0;
+		this.ecc = null;
+		this.maxNumOfBits = 0;
+	}
+	
 }
