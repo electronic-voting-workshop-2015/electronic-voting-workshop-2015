6a2b55751cfa0fe9e42bf5755b714cf37c645b7f
ronsinai <ron564@gmail.com>
2016-02-24 16:03:24 +0200

    Update Parameters.java

--- workshop/Parameters.java
+++ workshop/Parameters.java
@@ -19,6 +19,10 @@ public class Parameters {
 	public static int topQRLevel;
 	// The audit QR version
 	public static int bottomQRLevel;
+	// number of voting machines
+	public static int numOfMachines;
+	// maps machine to its signature
+	public static HashMap<Integer,byte[]> mapMachineToSignature;
 	// Set of the names of all the candidates in these elections - for the
 	// mapping.
 	public static HashSet<String> candidatesNames;
@@ -54,7 +58,7 @@ public class Parameters {
 
 	public static void setParameters(Group ourGroup1, ECClientCryptographyModule cryptoClient1, byte[] publicKey1, int topQRLevel1,
 			int bottomQRLevel1, HashSet<String> candidatesNames1,
-			ArrayList<RaceProperties> racesProperties1, int timeStampLevel1) {
+			ArrayList<RaceProperties> racesProperties1, int timeStampLevel1, int numOfMachines1) {
 		ourGroup = ourGroup1;
 		cryptoClient = cryptoClient1;
 		publicKey = publicKey1;
@@ -63,5 +67,36 @@ public class Parameters {
 		candidatesNames = candidatesNames1;
 		racesProperties = racesProperties1;
 		timeStampLevel = timeStampLevel1;
+		numOfMachines = numOfMachines1;
+		mapMachineToSignature = setMachinesSignatures();
+	}
+	
+	public static HashMap<Integer,byte[]> setMachinesSignatures() {
+		HashMap<Integer,byte[]> map = new HashMap<>();
+		boolean validSignature = true; // valid that each machine's signature is different from the previous ones
+		for (int i = 1; i <= numOfMachines; i++) {
+			byte[] signature = new byte[ourGroup.getElementSize()];
+			new Random().nextBytes(signature);
+			// loop to check that the signature of machine #i is different from all the previous machines' signatures
+			for (int j = 1; j < i; j++) {
+				if (sameArray(signature, map.get(j))) {
+					validSignature = false;
+					i--;
+					break;
+				}
+			}
+			if (validSignature)
+				map.put(i, signature);
+			validSignature = true;
+		}
+		return map;
+	}
+	
+	public static boolean sameArray(byte[] arr1, byte[] arr2) {
+		for (int i = 0; i < arr1.length; i++) {
+			if (arr1[i] != arr2[i])
+				return false;
+		}
+		return true;
 	}
 }
