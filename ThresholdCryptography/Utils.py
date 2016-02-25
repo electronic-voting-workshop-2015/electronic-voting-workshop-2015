e4ddea6487c2d15ddddccebc42195dd6a32ec14b
ilayluz <ilayluz@gmail.com>
2016-02-25 18:27:23 +0200

    Update Utils.py

--- ThresholdCryptography/Utils.py
+++ ThresholdCryptography/Utils.py
@@ -5,6 +5,7 @@ import urllib.request
 
 BB_API_ERROR = "error"  # TODO: define error message from BB
 
+
 def bits(n):
     """Generates binary digits of n, starting from least significant bit.
     from http://andrea.corbellini.name/2015/05/17/elliptic-curve-cryptography-a-gentle-introduction/
@@ -20,7 +21,26 @@ def split_every(n, iterable):
         yield iterable[i:i+n]
 
 
-def product(l, p = 0):
+def least_significant(num, n):
+    """returns the least significant (rightmost) n bits of num"""
+    mask = 2**n - 1
+    return num & mask
+
+
+def most_significant(num, n):
+    """returns the most significant (leftmost) n bits of num"""
+    length = num.bit_length()
+    mask = (2**n - 1) * 2**(length-n)
+    return num & mask
+
+
+def concat_bits(a, b, b_len):
+    """returns an a_len+b_len bits number formed from concatenating the binary
+    representations of a and b"""
+    return a * 2**b_len + b
+
+
+def product(l, p=0):
     """computes product of list, mod p if second passed second argument"""
     res = 1
     if p == 0:
@@ -81,12 +101,110 @@ def base64_to_list(b, member_length=0, curve=None):
     return bytes_to_list(base64_to_bytes(b), member_length, curve)
 
 
-def publish_list(l, int_length, sender_id, signature, table_id, recipient_id, url):
-    """publishes list l to url using POST request"""
-    base64_data = list_to_base64(l, int_length)
+def publish_list(list_data, int_length, sender_id, signature, table_id, recipient_id, url):
+    """publishes list l to url using POST request
+    if list is a list of group_members, int_length should be 0"""
+    base64_data = list_to_base64(list_data, int_length)
     json_data = json.dumps([sender_id, base64_data, signature, table_id, recipient_id])
     req = urllib.request.Request(url, json_data, {'Content-Type': 'application/json'})
     with urllib.request.urlopen(req) as response:
         read_value = response.read()  # read_value is a bytes object
     if read_value.decode('utf-8') == BB_API_ERROR:
         return None
+
+
+def mod_sqrt(a, p):
+    """ Find a quadratic residue (mod p) of 'a'. p
+        must be an odd prime.
+
+        Solve the congruence of the form:
+            x^2 = a (mod p)
+        And returns x. Note that p - x is also a root.
+
+        0 is returned is no square root exists for
+        these a and p.
+
+        The Tonelli-Shanks algorithm is used (except
+        for some simple cases in which the solution
+        is known from an identity). This algorithm
+        runs in polynomial time (unless the
+        generalized Riemann hypothesis is false).
+
+        from http://eli.thegreenplace.net/2009/03/07/computing-modular-square-roots-in-python
+    """
+    # Simple cases
+    #
+    if legendre_symbol(a, p) != 1:
+        return 0
+    elif a == 0:
+        return 0
+    elif p == 2:
+        return p
+    elif p % 4 == 3:
+        return pow(a, (p + 1) // 4, p)
+
+    # Partition p-1 to s * 2^e for an odd s (i.e.
+    # reduce all the powers of 2 from p-1)
+    #
+    s = p - 1
+    e = 0
+    while s % 2 == 0:
+        s //= 2
+        e += 1
+
+    # Find some 'n' with a legendre symbol n|p = -1.
+    # Shouldn't take long.
+    #
+    n = 2
+    while legendre_symbol(n, p) != -1:
+        n += 1
+
+    # Here be dragons!
+    # Read the paper "Square roots from 1; 24, 51,
+    # 10 to Dan Shanks" by Ezra Brown for more
+    # information
+    #
+
+    # x is a guess of the square root that gets better
+    # with each iteration.
+    # b is the "fudge factor" - by how much we're off
+    # with the guess. The invariant x^2 = ab (mod p)
+    # is maintained throughout the loop.
+    # g is used for successive powers of n to update
+    # both a and b
+    # r is the exponent - decreases with each update
+    #
+    x = pow(a, (s + 1) // 2, p)
+    b = pow(a, s, p)
+    g = pow(n, s, p)
+    r = e
+
+    while True:
+        t = b
+        m = 0
+        for m in range(r):
+            if t == 1:
+                break
+            t = pow(t, 2, p)
+
+        if m == 0:
+            return x
+
+        gs = pow(g, 2 ** (r - m - 1), p)
+        g = (gs * gs) % p
+        x = (x * gs) % p
+        b = (b * g) % p
+        r = m
+
+
+def legendre_symbol(a, p):
+    """ Compute the Legendre symbol a|p using
+        Euler's criterion. p is a prime, a is
+        relatively prime to p (if p divides
+        a, then a|p = 0)
+
+        Returns 1 if a has a square root modulo
+        p, -1 otherwise.
+    """
+    ls = pow(a, (p - 1) // 2, p)
+    return -1 if ls == p - 1 else ls
