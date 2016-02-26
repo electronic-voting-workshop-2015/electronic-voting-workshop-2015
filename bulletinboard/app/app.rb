b376a5b0c1f4504009f2563bc482269c166d3462
jschejter <jschejter@gmail.com>
2016-02-26 14:53:46 +0200

    getBBVotes

--- bulletinboard/app/app.rb
+++ bulletinboard/app/app.rb
@@ -13,13 +13,26 @@ post '/sendVote' do
   #todo: verify signature
   
   for index in 0 ... params['votes'].size-1
-    create = votes.create(vote_value: params['votes'].fetch(index)[vote_value],
+    create = votes.create(vote_value: params['votes'].fetch(index)['vote_value'],
                           ballot_box: params['ballot_box'].to_i,
                           serial_number: params['serial_number'].to_i,
                           race_id: index+1)
   end
   
 end
+
+get '/getBBVotes' do
+  
+  input_race_id = parmas['race_id']    
+    
+  if race_id <> -1
+    votes_hash = votes.where(race_id: input_race_id)
+  else
+    votes_hash = votes.find(:all)
+  
+  votes_hash.to_json(:except => :updated_at)
+end
+
 
 post '/publish' do
     publication = Publication.create!( content: params[ "content" ] )
