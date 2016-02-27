5f73a800d718eee6a3e508f8878d3930b0608f27
jschejter <jschejter@gmail.com>
2016-02-27 20:50:43 +0200

    added more tests to votes

--- bulletinboard/test/test_votes.rb
+++ bulletinboard/test/test_votes.rb
@@ -11,6 +11,8 @@ class VotesAPITest < MiniTest::Unit::TestCase
     
     post "/sendVote", ballot_box: 4, serial_number: 12345, votes: [{vote_value: "qwert"}, {vote_value: "qazwsx"}]
     assert last_response.ok?
+	post "/sendVote", ballot_box: 2, serial_number: 54321, votes: [{vote_value: "jjjj"}, {vote_value: "vvvv"}]
+    assert last_response.ok?
 
     get "/getBBVotes", race_id: -1
     assert last_response.ok?
@@ -22,6 +24,19 @@ class VotesAPITest < MiniTest::Unit::TestCase
     assert_equal ["12345"],            parsed[ 0 ][ "serial_number" ]
 	assert_equal ["qwert", "qazwsx"],     parsed[ 0 ][ "votes" ]
 
+
+    get "/getBBVotes", race_id: 2
+    assert last_response.ok?
+    parsed = JSON.parse last_response.body
+    assert_equal 2, parsed.count
+
+    assert_equal ["balot_box", "serial_number", "votes"],  parsed[ 0 ].keys
+	assert_equal ["4"],      parsed[ 0 ][ "ballot_box" ]
+    assert_equal ["2"],      parsed[ 1 ][ "ballot_box" ]
+    assert_equal ["12345"],            parsed[ 0 ][ "serial_number" ]
+	assert_equal ["54321"],            parsed[ 1 ][ "serial_number" ]
+	assert_equal ["qwert"],     parsed[ 0 ][ "votes" ]
+	assert_equal ["vvvv"],     parsed[ 1 ][ "votes" ]
 
   end
 
