214ebbfeb0e606851787035f87361ee9e177d11d
sofiarov <srsoniarov@gmail.com>
2016-03-04 22:07:25 +0200

    Update QRProperties.java

--- workshop/QRProperties.java
+++ workshop/QRProperties.java
@@ -4,50 +4,42 @@ import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 
 public class QRProperties {
 
-	private int level;
-	private ErrorCorrectionLevel ecc;
-	private int maxNumOfBits;
-	private int numberOfPixels;
-
-	public int getNumberOfPixels() {
-		return this.numberOfPixels;
-		//TODO: calculate number of pixels according to QR level.
-	}
-
-	public int getLevel() {
-		return level;
-	}
-
-	public void setLevel(int level) {
-		this.level = level;
-	}
+	private final ErrorCorrectionLevel ecc;
+	private final int width;
+	private final int height;
 
 	public ErrorCorrectionLevel getEcc() {
 		return ecc;
 	}
-
-	public void setEcc(ErrorCorrectionLevel ecc) {
-		this.ecc = ecc;
-	}
-
-	public int getMaxNumOfBits() {
-		return maxNumOfBits;
-	}
-
-	public void setMaxNumOfBits(int maxNumOfBits) {
-		this.maxNumOfBits = maxNumOfBits;
-	}
-
-	public QRProperties(int level, ErrorCorrectionLevel ecc, int maxNumOfBits) {
-		this.level = level;
-		this.ecc = ecc;
-		this.maxNumOfBits = maxNumOfBits;
-	}
-
-	public QRProperties() {
-		this.level = 0;
-		this.ecc = null;
-		this.maxNumOfBits = 0;
+	
+	public int getWidth() {
+		return width;
 	}
 	
+	public int getHeight() {
+		return height;
+	}
+
+	public QRProperties(int numberOfBytes, int qrWidth, int qrHeight) {
+		this.ecc = calculateECC(numberOfBytes);
+		this.width = qrWidth;
+		this.height = qrHeight;
+	}
+
+	/*
+	 * Returns the highest possible ECC for the given number of bytes,
+	 */
+	static private ErrorCorrectionLevel calculateECC(int numberOfBytes) {
+		if (numberOfBytes < 890) {
+			return ErrorCorrectionLevel.H;
+		}
+		if (numberOfBytes < 1160) {
+			return ErrorCorrectionLevel.Q;
+		}
+		if (numberOfBytes < 1620) {
+			return ErrorCorrectionLevel.M;
+		} // >= 1620
+		return ErrorCorrectionLevel.L;
+	}
+
 }
