8db6441939e131a9b65c8d3f9a5b448f61c2a7c3
EliranAbdoo <eliranabdoo@gmail.com>
2016-04-13 21:12:42 +0300

    Update Main.java

--- workshop/Main.java
+++ workshop/Main.java
@@ -2,7 +2,7 @@ package workshop;
 
 import org.json.*;
 
-public class Main {
+/**public class Main {
 
 	public static void main(String[] args) throws JSONException {
 		Parameters.init();
@@ -13,6 +13,35 @@ public class Main {
 			obj.vote(jsonArray);
 		} else
 			obj.audit(Boolean.valueOf(((JSONObject) jsonArray.get(0)).getBoolean("audit")));
+	}
+
+}OLD MAIN, NO HEBREW**/ 
+
+
+public class Main {
+	
+	public static String decodeJson(String s){
+		String res="";
+		String c= new String(s);
+		String[] a=c.split(",");
+		for(int i=0; i<a.length; i++){
+			res+=(char)(Integer.parseInt(a[i]));
+		}
+		return res;
+	}
+
+	public static void main(String[] args) throws JSONException {
+		Parameters.init();				
+		VotingBoothImp obj = new VotingBoothImp();
+		if (args[0].equals("vote")) {
+			String jsonString=decodeJson(args[1]);
+			JSONArray jsonArray = new JSONArray(jsonString);
+			jsonArray.remove(0); // Remove mchine id.
+			obj.vote(jsonArray);
+		} else{
+			JSONArray jsonArray = new JSONArray(args[1]);
+			obj.audit(Boolean.valueOf(((JSONObject) jsonArray.get(0)).getBoolean("audit")));
+		}
 	}
 
 }
