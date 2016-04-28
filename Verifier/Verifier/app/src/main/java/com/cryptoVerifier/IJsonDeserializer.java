c290922ce8f1123d3b406f7e43101fcfca283e6f
Yemini Ori <yeminiori@gmail.com>
2016-04-28 17:59:51 +0300

    .

--- Verifier/Verifier/app/src/main/java/com/cryptoVerifier/IJsonDeserializer.java
+++
@@ -1,9 +0,0 @@
-package com.cryptoVerifier;
-
-/**
- * Created by Maor Elias on 25/02/16.
- */
-public interface IJsonDeserializer<T> {
-
-    T deserialize(String json);
-}
--- Verifier/Verifier/app/src/main/java/com/cryptoVerifier/MixnetVerifierUtils/MixnetProofsContainer.java
+++
@@ -1,9 +0,0 @@
-package com.cryptoVerifier.MixnetVerifierUtils;
-
-/**
- * Created by Maor Elias on 25/02/16.
- */
-public class MixnetProofsContainer {
-
-    public MixnetProofs content;
-}
